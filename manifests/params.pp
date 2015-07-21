18d63a2b0d2f248ce376bd13e4c07c4ea1f03e90
Hrachos <hrachos@inuits.eu>
2015-07-21 14:23:21 +0200

    imlicit hiera lookup for puppetdb
    Signed-off-by: Hrachos <hrachos@inuits.eu>

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,11 +2,12 @@
 #
 # This class contains every parameter which will be used in this puppet module
 # for setting up the ansible service
-class ansible::params {
-  $pkg_name         = 'ansible'
-  $pkg_version      = 'installed'
-  $remote_port      = '22'
-  $puppetdb_host    = hiera('puppetdb_host')
-  $domain           = '\'.*\''
-  $puppet_inventory = true
-}
+class ansible::params (
+  $pkg_name         = 'ansible',
+  $pkg_version      = 'installed',
+  $remote_port      = '22',
+  $puppetdb_host    = undef,
+  $domain           = '\'.*\'',
+  $puppet_inventory = true,
+)
+{}
