812b505f06103d2ac025c455a01cccd06bde67bd
Hrachos <hrachos@inuits.eu>
2015-07-22 16:19:26 +0200

    params parameters replaced by variables
    Signed-off-by: Hrachos <hrachos@inuits.eu>

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,12 +2,11 @@
 #
 # This class contains every parameter which will be used in this puppet module
 # for setting up the ansible service
-class ansible::params (
-  $pkg_name         = 'ansible',
-  $pkg_version      = 'installed',
-  $remote_port      = '22',
-  $puppetdb_host    = undef,
-  $domain           = '\'.*\'',
-  $puppet_inventory = true,
-)
-{}
+class ansible::params {
+  $pkg_name         = 'ansible'
+  $pkg_version      = 'installed'
+  $remote_port      = '22'
+  $puppetdb_host    = undef
+  $domain           = '\'.*\''
+  $puppet_inventory = true
+}
